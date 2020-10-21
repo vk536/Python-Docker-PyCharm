7a43ea8dd08e317ce9edec2dccc831dc6129194d
vivek <vk536@njit.edu>
2020-10-20 23:12:00 -0400

    Feature: Modifying code

--- src/my_script.py
+++ src/my_script.py
@@ -3,5 +3,5 @@
 from pystrich.datamatrix import DataMatrixEncoder
 
 encoder = DataMatrixEncoder('This is a DataMatrix.')
-encoder.save('./datamatrix_test.png')
+encoder.save('./src/datamatrix_test.png')
 print(encoder.get_ascii())
